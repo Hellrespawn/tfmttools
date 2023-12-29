8081a1ce56128eb573cca46c9019bdcdd55ebab5
Stef Korporaal <stef@skorporaal.nl>
2023-12-29 15:54:18 +0100

    Remove TUI

--- tfmttools-tui/src/util.rs
+++
@@ -1,5 +0,0 @@
-pub fn rows_required_for_string(string: &str, width: usize) -> usize {
-    string.lines().fold(0, |acc, el| {
-        acc + console::measure_text_width(el).div_ceil(width)
-    })
-}
